5e0421e78658a1c36b5c325bd8dd45c10f6f2050
Arya Syamudra <100120042+Arya-Syamudra@users.noreply.github.com>
2025-08-12 09:46:38 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
-from setuptools import setup, Extension
 import sys
+import sysconfig
+from setuptools import setup, Extension
 import pybind11
 import os
-from setuptools.command.build_ext import build_ext
 
-opencv_includes = []  # let compiler find OpenCV via pkg-config
-opencv_libs = []
+# Lokasi header OpenCV di Ubuntu / Colab
+opencv_include = "/usr/include/opencv4"
+
+# Library OpenCV yang dibutuhkan
+opencv_libs = ["opencv_core", "opencv_imgcodecs", "opencv_imgproc"]
 
 ext_modules = [
     Extension(
         "lsbm_features",
         sources=["src/bindings.cpp", "src/lsbm_features.cpp"],
-        include_dirs=[pybind11.get_include(), "include"],
+        include_dirs=[
+            pybind11.get_include(),
+            pybind11.get_include(user=True),
+            opencv_include,
+            "include"
+        ],
+        libraries=opencv_libs,
         language="c++",
         extra_compile_args=["-std=c++17"]
     )
@@ -20,8 +29,10 @@ ext_modules = [
 setup(
     name="lsbm_features",
     version="0.1.0",
-    description="LSBM features (Liu 2006) - C++ implementation",
+    author="Your Name",
+    author_email="you@example.com",
+    description="LSB Matching Steganalysis RGB Feature Extractor (Liu 2006)",
     ext_modules=ext_modules,
     install_requires=["pybind11", "opencv-python"],
+    zip_safe=False,
 )
-
